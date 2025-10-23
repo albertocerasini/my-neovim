39d3b5c5d6bc9fc471ab8a675ae25f11f8af41c6
albertocerasini <alberto.cerasini@gmail.com>
2025-10-23 15:23:56 +0200

    added rustceanvim plugin

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -2,11 +2,15 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^6', -- Recommended
   lazy = false, -- This plugin is already lazy
-  opts = {
-    tools = {
-      hover_actions = {
-        replace_builtin_hover = true,
+  ft = { 'rust' }, -- Load when opening Rust files
+  init = function()
+    -- Configure rustaceanvim here
+    vim.g.rustaceanvim = {
+      tools = {
+        hover_actions = {
+          replace_builtin_hover = true,
+        },
       },
-    },
-  },
+    }
+  end,
 }
